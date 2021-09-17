ac928398caf1893de3b2e4b978fb809fa3515f37
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-17 20:47:12 +0300

    Add metadata: add metadata to main page
    Added description, tags information, title and other fields

--- app.py
+++ app.py
@@ -2,7 +2,68 @@ from fastapi import FastAPI, status
 
 from routers import player, road, country, item, city, heroclass, horse, admin, authentication
 
-app = FastAPI()
+description = """
+Telegramia API helps you do awesome stuff. 🚀
+
+## All items
+
+You will be able to:
+
+* **Read items** (without _authentication_).
+* **Create, update and delete items** (with _authentication_).
+"""
+
+tags_metadata = [
+    {
+        'name': 'Admins',
+        'description': 'Operations with admins. They have full access in this API.',
+    },
+    {
+        'name': 'Cities',
+        'description': 'Operations with cities. This are main objects in game.',
+    },
+    {
+        'name': 'Countries',
+        'description': 'Operations with countries. They summarize all objects in game world.',
+    },
+    {
+        'name': 'Hero classes',
+        'description': 'Operations with classes. They describe different classes that player can choose.',
+    },
+    {
+        'name': 'Horses',
+        'description': 'Operations with horses. They are main transport in game world.',
+    },
+    {
+        'name': 'Items',
+        'description': 'Operations with items. They help players to survive in game world.',
+    },
+    {
+        'name': 'Players',
+        'description': 'Operations with players. Information about all players in game.',
+    },
+    {
+        'name': 'Roads',
+        'description': 'Operations with roads. They connect all objects in game world.',
+    },
+]
+
+app = FastAPI(
+    title='Telegramia API',
+    description=description,
+    version='1.0.0',
+    terms_of_service='https://github.com/mezgoodle/Telegramia-API/blob/master/CODE_OF_CONDUCT.md',
+    contact={
+        'name': 'Maxim Zavalniuk',
+        'url': 'https://github.com/mezgoodle',
+        'email': 'mezgoodle@gmail.com',
+    },
+    license_info={
+        'name': 'MIT License',
+        'url': 'https://github.com/mezgoodle/Telegramia-API/blob/master/LICENSE',
+    },
+    openapi_tags=tags_metadata
+)
 
 app.include_router(authentication.router)
 app.include_router(player.router)
