b23a2df173e1785c7fb2b8be67abe45c034e0be4
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-18 17:18:28 +0300

    Update config.py
    Fix errors with naming

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'db_password')
 SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 ALGORITHM = os.getenv('ALGORITHM', 'type')
-ADMIN_NICKNAME = os.getenv('NICKNAME', 'nickname')
-ADMIN_PASSWORD = os.getenv('NICKNAME', 'password')
+ADMIN_NICKNAME = os.getenv('ADMIN_NICKNAME', 'nickname')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'password')
