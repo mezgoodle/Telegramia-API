578668eacf550d8a9c2ae6d234088b367e247d7b
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-18 16:50:09 +0200

    Update test_admin.py

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -7,54 +7,52 @@ client = TestClient(app)
 
 
 def test_get_admins():
-    response = client.get('/admins')
+    response = client.get("/admins")
     assert response.status_code == 200
 
 
 def test_get_admin():
-    response = client.get('/admin?admin_name=mezgoodle')
+    response = client.get("/admin?admin_name=mezgoodle")
     assert response.status_code == 200
 
 
 def test_post_admin():
     token_response = client.post(
-        '/login',
-        data={'username': ADMIN_NICKNAME, 'password': ADMIN_PASSWORD})
-    access_token = token_response.json()['access_token']
+        "/login", data={"username": ADMIN_NICKNAME, "password": ADMIN_PASSWORD}
+    )
+    access_token = token_response.json()["access_token"]
     response = client.post(
-        '/admin',
+        "/admin",
         json={
             "name": "mezgoodle111",
             "email": "mezgoodle@gmail.com",
-            "password": "123456"
+            "password": "123456",
         },
-        headers={'Authorization': f'Bearer {access_token}'}
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 201
 
 
 def test_update_admin():
     token_response = client.post(
-        '/login',
-        data={'username': ADMIN_NICKNAME, 'password': ADMIN_PASSWORD})
-    access_token = token_response.json()['access_token']
+        "/login", data={"username": ADMIN_NICKNAME, "password": ADMIN_PASSWORD}
+    )
+    access_token = token_response.json()["access_token"]
     response = client.put(
-        '/admin?admin_name=mezgoodle',
-        json={
-            "name": "mezgoodle"
-        },
-        headers={'Authorization': f'Bearer {access_token}'}
+        "/admin?admin_name=mezgoodle111",
+        json={"name": "mezgoodle"},
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 201
 
 
 def test_delete_admin():
     token_response = client.post(
-        '/login',
-        data={'username': ADMIN_NICKNAME, 'password': ADMIN_PASSWORD})
-    access_token = token_response.json()['access_token']
+        "/login", data={"username": ADMIN_NICKNAME, "password": ADMIN_PASSWORD}
+    )
+    access_token = token_response.json()["access_token"]
     response = client.delete(
-        '/admin?admin_name=mezgoodle111',
-        headers={'Authorization': f'Bearer {access_token}'}
+        "/admin?admin_name=mezgoodle111",
+        headers={"Authorization": f"Bearer {access_token}"},
     )
     assert response.status_code == 204
