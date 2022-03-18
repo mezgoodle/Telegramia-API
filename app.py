9123a5306e4abf9a4d8a8ec7492e1639fe9eb444
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-18 16:23:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI, status
 
-from routers import player, road, country, item, city, heroclass, horse, dungeon, admin, authentication
+from routers import (
+    player,
+    road,
+    country,
+    item,
+    city,
+    heroclass,
+    horse,
+    dungeon,
+    admin,
+    authentication,
+    raid,
+)
 
 description = """
 Telegramia API helps you do awesome stuff. 🚀
@@ -15,58 +27,58 @@ You will be able to:
 
 tags_metadata = [
     {
-        'name': 'Admins',
-        'description': 'Operations with admins. They have full access in this API.',
+        "name": "Admins",
+        "description": "Operations with admins. They have full access in this API.",
     },
     {
-        'name': 'Cities',
-        'description': 'Operations with cities. This are main objects in game.',
+        "name": "Cities",
+        "description": "Operations with cities. This are main objects in game.",
     },
     {
-        'name': 'Countries',
-        'description': 'Operations with countries. They summarize all objects in game world.',
+        "name": "Countries",
+        "description": "Operations with countries. They summarize all objects in game world.",
     },
     {
-        'name': 'Hero classes',
-        'description': 'Operations with classes. They describe different classes that player can choose.',
+        "name": "Hero classes",
+        "description": "Operations with classes. They describe different classes that player can choose.",
     },
     {
-        'name': 'Horses',
-        'description': 'Operations with horses. They are main transport in game world.',
+        "name": "Horses",
+        "description": "Operations with horses. They are main transport in game world.",
     },
     {
-        'name': 'Items',
-        'description': 'Operations with items. They help players to survive in game world.',
+        "name": "Items",
+        "description": "Operations with items. They help players to survive in game world.",
     },
     {
-        'name': 'Players',
-        'description': 'Operations with players. Information about all players in game.',
+        "name": "Players",
+        "description": "Operations with players. Information about all players in game.",
     },
     {
-        'name': 'Roads',
-        'description': 'Operations with roads. They connect all objects in game world.',
+        "name": "Roads",
+        "description": "Operations with roads. They connect all objects in game world.",
     },
     {
-        'name': 'Dungeons',
-        'description': 'Operations with dungeons. Here players can earn money and experiments.',
+        "name": "Dungeons",
+        "description": "Operations with dungeons. Here players can earn money and experiments.",
     },
 ]
 
 app = FastAPI(
-    title='Telegramia API',
+    title="Telegramia API",
     description=description,
-    version='1.1.1',
-    terms_of_service='https://github.com/mezgoodle/Telegramia-API/blob/master/CODE_OF_CONDUCT.md',
+    version="1.1.1",
+    terms_of_service="https://github.com/mezgoodle/Telegramia-API/blob/master/CODE_OF_CONDUCT.md",
     contact={
-        'name': 'Maxim Zavalniuk',
-        'url': 'https://github.com/mezgoodle',
-        'email': 'mezgoodle@gmail.com',
+        "name": "Maxim Zavalniuk",
+        "url": "https://github.com/mezgoodle",
+        "email": "mezgoodle@gmail.com",
     },
     license_info={
-        'name': 'MIT License',
-        'url': 'https://github.com/mezgoodle/Telegramia-API/blob/master/LICENSE',
+        "name": "MIT License",
+        "url": "https://github.com/mezgoodle/Telegramia-API/blob/master/LICENSE",
     },
-    openapi_tags=tags_metadata
+    openapi_tags=tags_metadata,
 )
 
 app.include_router(authentication.router)
@@ -79,8 +91,9 @@ app.include_router(heroclass.router)
 app.include_router(horse.router)
 app.include_router(dungeon.router)
 app.include_router(admin.router)
+app.include_router(raid.router)
 
 
-@app.get('/', status_code=status.HTTP_200_OK)
+@app.get("/", status_code=status.HTTP_200_OK)
 async def start_page():
-    return {'Information': 'Too see all available methods visit /docs page'}
+    return {"Information": "Too see all available methods visit /docs page"}
